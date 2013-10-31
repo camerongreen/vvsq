85f74dfb1708eb7c1f79ad2cc2fa3c05bd36a227
Cameron Green <camerongreen@gmail.com>
2013-10-31 13:31:25 +1000

    Styling page image, fixing fb image

--- modules/vvsq_blocks/theme/social-media.tpl.php
+++ modules/vvsq_blocks/theme/social-media.tpl.php
@@ -1,5 +1,5 @@
 <div id="social-media">
   <ul>
-    <li><a href="https://www.facebook.com/pages/Vegetarian-and-Vegan-Society-of-Queensland/151360723773" target="_blank" title="VVSQ Facebook Page"><img src="<?php print path_to_theme(); ?>/theme/images/fb.png" alt="Facebook Logo" /></a></li>
+    <li><a href="https://www.facebook.com/pages/Vegetarian-and-Vegan-Society-of-Queensland/151360723773" target="_blank" title="VVSQ Facebook Page"><img src="/<?php print path_to_theme(); ?>/theme/images/fb.png" alt="Facebook Logo" /></a></li>
   </ul>
 </div>
