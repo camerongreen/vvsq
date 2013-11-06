22000dd7f87180a2160cde30d8655a78a9124bfd
Cameron Green <camerongreen@gmail.com>
2013-11-06 23:54:00 +1000

    Adding new subtheme of coworker

---
+++ themes/vegsoc/js/scripts.js
@@ -0,0 +1,6 @@
+/**
+ *
+ * User: Cameron Green <i@camerongreen.org>
+ * Date: 06/11/13
+ * Time: 23:51
+ */
---
+++ themes/vegsoc/template.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cameron
+ * Date: 06/11/13
+ * Time: 20:27
+ */ 
