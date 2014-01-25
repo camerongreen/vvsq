4c27a616bbcd497284b5471d5a81a2f91a63e767
Cameron Green <camerongreen@gmail.com>
2014-01-25 23:34:04 +1000

    Adding maps to restaurant view, and auto geolocation

--- modules/vvsq_restaurant_maps/theme/restaurant-map.tpl.php
+++ modules/vvsq_restaurant_maps/theme/restaurant-map.tpl.php
@@ -1,3 +1,20 @@
+<script type="text/javascript">
+  function initialize() {
+    // obligatory Google help file naming
+    var myLatLng = new google.maps.LatLng(<?= $lat; ?>, <?= $lng; ?>);
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById("qld-map"),
+      mapOptions);
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: '<?= $node->title; ?>'
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 <div id="qld-map">
-  <h1>I am a map for node <?= $node->nid; ?></h1>
 </div>
